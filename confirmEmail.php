669dccf91d93af5f86eafbe7afeba5191e0d5910
arxk0430 <ky1106199942@gmail.com>
2021-03-17 16:16:34 +0800

    Add files via upload

---
+++ confirmEmail.php
@@ -0,0 +1,17 @@
+<head>
+	<script>
+		function confirm(token, tokenId){
+			try{
+				await app.emailPasswordAuth.confirmUser(token, tokenId);
+			} catch(error) {
+				alert(error)
+			}
+		}
+	</script>
+</head>
+<body
+<?
+	echo 'onload="confirm('.$_GET["token",].", ".$_GET["tokenId"].')"'
+?>
+>
+</body>
