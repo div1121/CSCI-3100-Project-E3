7b9c1363d92c5bf1f3af2f56323360c0a60c1787
div1121 <jasonchu19991121@gmail.com>
2021-03-19 15:33:08 +0800

    Update Gameroom.js

--- src_gameroom/Gameroom.js
+++ src_gameroom/Gameroom.js
@@ -6,10 +6,13 @@ class Playerline extends Component{
         if (this.props.isready){
             str = "Cancel"
         }
-        return (<div>
-            {this.props.playername}
-            <button onClick={this.props.handleready}>{str}</button>
-        </div>)
+        return (<tr>
+            <td>
+            {this.props.playername}</td>
+            <td><button onClick={this.props.handleready}>{str}</button>
+            </td>
+            </tr>
+            )
     }
 }
 
@@ -17,6 +20,8 @@ class Gameroom extends Component{
     constructor(props) {
         super(props);
         this.state = {room_name: this.props.roomname, player_list: this.props.player_list, player_num: this.props.player_num, ready_num: 0 ,ready_state: Array(4).fill(false)};
+        this.addready = this.addready.bind(this)
+        this.minusready = this.minusready.bind(this)
     }
 
     addready(i){
@@ -48,14 +53,16 @@ class Gameroom extends Component{
         }
         return (<div>
             <h1>{this.props.room_name}</h1>
-            <Playerline isready={this.state.ready_state[0]}
+            <table>
+                <Playerline isready={this.state.ready_state[0]}
                 playername={this.props.player_list[0]} handleready={list[0]} />
             <Playerline isready={this.state.ready_state[1]}
                         playername={this.props.player_list[1]} handleready={list[1]} />
-            <Playerline isready={this.state.ready_state[2]}
+                <Playerline isready={this.state.ready_state[2]}
                         playername={this.props.player_list[2]} handleready={list[2]} />
-            <Playerline isready={this.state.ready_state[3]}
+                <Playerline isready={this.state.ready_state[3]}
                         playername={this.props.player_list[3]} handleready={list[3]} />
+            </table>
             <h2>Number of ready: {this.state.ready_num}</h2>
         </div>)
     }
