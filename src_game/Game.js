d8aeec7f7db564d67139f15e3f5862e462546a1a
bennyclh <80705203+bennyclh@users.noreply.github.com>
2021-04-14 22:27:58 +0800

    Update Game.js

--- src_game/Game.js
+++ src_game/Game.js
@@ -144,46 +144,10 @@ class Game extends Component {
     }
 
     startGame() {
-        this.setEntrances()
         this.setRanking()
     }
 
-    setEntrances() {
-        let {
-            randomEntrances,
-            boardHeight,
-            boardWidth
-        } = this.state
-        let randomValues
-        for (let i = 0; i < boardHeight; i++) {
-            for (let j = 0; j < boardWidth; j++) {
-                randomEntrances.push([])
-                let entranceDifferences = [[1, -1], [1, 0], [0, 1], [-1, 1]]
-                let temp = 4
-                while (temp > 0) {
-                    randomValues = Math.floor(Math.random() * temp)
-                    let targetWidth = j + entranceDifferences[randomValues][0]
-                    let targetHeight = i + entranceDifferences[randomValues][1]
-                    if (targetWidth < 0 || targetWidth >= boardHeight || targetHeight < 0 || targetHeight >= boardHeight) {
-                        randomEntrances[j + i * boardWidth].push([])
-                        randomEntrances[j + i * boardWidth][4 - temp].push(j)
-                        randomEntrances[j + i * boardWidth][4 - temp].push(i)
-                    }
-                    else {
-                        randomEntrances[j + i * boardWidth].push([])
-                        randomEntrances[j + i * boardWidth][4 - temp].push(targetWidth)
-                        randomEntrances[j + i * boardWidth][4 - temp].push(targetHeight)
-                    }
-                    temp--
-                    entranceDifferences.splice(randomValues, 1)
-                }
-            }
-        }
-        this.setState({
-            randomEntrances
-        })
-    }
-
+    
     countTotalMoves() {
         this.setState({
             totalMoves: ++this.state.totalMoves
