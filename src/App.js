92fcbca1ed760480bbb0cf0a68bac63f330d69df
arxk0430 <ky1106199942@gmail.com>
2021-04-15 13:26:42 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { TopNavbar } from './Navbar'
+import News from './News';
 import Menu from './Menu';
 import Chatroom from './ChatRoom';
 import RoomList from "./RoomList";
 import GameRoom from './GameRoom';
 import Game from './Game';
+import Demo from './Demo';
 import HomeButton from './HomeButton';
 
 function App() {
@@ -18,7 +20,7 @@ function App() {
 	return (
 		<div className="app">
 			{mode === "Game"?
-				<div className="game"><Game userid={userID} roomid={gameRoomID}/></div>
+				<div className="game"><Game userid={userID} roomid={gameRoomID} /></div>
 			:
 			<>
 				<div className="appHeader">
@@ -26,18 +28,26 @@ function App() {
 					<HomeButton setMode={setMode}/>
 				</div>
 				<div className="appBody">
-							{gameRoomID !== null?
-							<>
-								<GameRoom setMode={setMode} roomid={gameRoomID} roomname={gameRoomEnter} playername={username} playerid={userID} setGameroomenter={setGameRoomEnter} setGameroomid={setGameRoomID}/>
-								<Chatroom roomid={gameRoomID} userid={userID} name={username} />
-							</>
-							:mode === "FindingRoom"?
-								<RoomList user_id={userID} user_name={username} setGameroomenter={setGameRoomEnter} setGameroomid={setGameRoomID}/>
-							:mode === "Demo"?
-								<Game />
-							:
-								<Menu setMode={setMode} userID={userID} username={username} setGameRoomEnter={setGameRoomEnter} setGameRoomID={setGameRoomID} />
-							}
+					{mode==="Home"?
+						<div className="homepage">
+							<News />
+							<Menu setMode={setMode} userID={userID} username={username} setGameRoomEnter={setGameRoomEnter} setGameRoomID={setGameRoomID} />
+						</div>
+					:gameRoomID !== null?
+						<>
+							<GameRoom setMode={setMode} roomid={gameRoomID} roomname={gameRoomEnter} playername={username} playerid={userID} setGameroomenter={setGameRoomEnter} setGameroomid={setGameRoomID}/>
+							<Chatroom roomid={gameRoomID} userid={userID} name={username} />
+						</>
+					:mode === "FindingRoom"?
+						<RoomList user_id={userID} user_name={username} setGameroomenter={setGameRoomEnter} setGameroomid={setGameRoomID}/>
+					:mode === "Demo"?
+						<div className="game"><Demo /></div>
+					:
+						<div className="homepage">
+							<News />
+							<Menu setMode={setMode} userID={userID} username={username} setGameRoomEnter={setGameRoomEnter} setGameRoomID={setGameRoomID} />
+						</div>
+					}
 				</div>
 			</>
 			}
