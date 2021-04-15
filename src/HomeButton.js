42b190ba536a8c436961df3b9997ef76f4a8da30
arxk0430 <ky1106199942@gmail.com>
2021-04-15 16:34:31 +0800

    Add files via upload

--- src/HomeButton.js
+++ src/HomeButton.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button } from '@material-ui/core';
 
 function HomeButton({ setMode }) {
 	const home = () => {
 		setMode("Home");
 	}
-	return <button size="lg" variant="primary" onClick={home}>Home</button>
+	return <Button variant="contained" color="default" onClick={home}>Home</Button>
 }
 
 export default HomeButton;
