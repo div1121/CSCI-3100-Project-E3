e8111865201f74a9f0a7a3b0ac05b087ec53a295
arxk0430 <ky1106199942@gmail.com>
2021-04-15 13:26:59 +0800

    Add files via upload

---
+++ src/News.js
@@ -0,0 +1,20 @@
+function News() {
+	return(
+		<div className='news'>
+			<div>
+				<h1>Check out the news about Magic Maze!</h1>
+			</div>
+			<div className="newsPiece">
+				<h4>16/4/2021: Today is our demonstration😆</h4>
+			</div>
+			<div className="newsPiece">
+				<h4>19/3/2021: Initial code deadline today!</h4>
+			</div>
+			<div className="newsPiece">
+				<h4>19/2/2021: Magic maze start developing~</h4>
+			</div>
+		</div>
+	);
+}
+
+export default News;
